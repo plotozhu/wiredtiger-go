12ebdf61be71d2a791aca1b3d342b94066cad987
jason jiang <czjs2@qq.com>
2021-07-21 02:13:13 +0000

    Update install-deps.sh
    update sh

--- install-deps.sh
+++ install-deps.sh
@@ -6,11 +6,12 @@ mkdir -p $SHELL_FOLDER/wdlibs/libs
 
 cd $SHELL_FOLDER/wdlibs
 if [ ! -d $SHELL_FOLDER/wdlibs/wiredtiger ]; then
-  git clone https://github.com/wiredtiger/wiredtiger.git
+  git clone https://github.com/wiredtiger/wiredtiger.git -b mongodb-3.6
   cd  wiredtiger
 
 else
     cd  wiredtiger
+    git checkout mongodb-3.6
     git pull https://github.com/wiredtiger/wiredtiger.git
 
 fi
